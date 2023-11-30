f21bf8edce7b24ce9f1ffbe3c7f478ddfacc6843
Micael Die <micaeldie@gmail.com>
2023-11-30 16:45:42 +0000

    initial nextjs app with tailwind, daisyui

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center underline">Next 13</h1>
+      <button className="btn">click me</button>
+    </main>
+  )
+}
