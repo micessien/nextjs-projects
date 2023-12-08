a0907d86f1ba24714e24cb51062024f7d33d8e43
Micael Die <micaeldie@gmail.com>
2023-12-08 13:35:39 +0000

    Design button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="py-4 px-48">
+      <Link href={"/dashboard"} className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md">Go to the dashboard</Link>
     </main>
   )
 }
