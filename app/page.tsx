f15be1f318481806ec0fb9113eb499703b3edf14
Micael Die <micaeldie@gmail.com>
2023-12-08 13:29:37 +0000

    Initialize fullstack next typescript app

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-import { getAllTodos } from "@/api"
-import AddTask from "./components/AddTask"
-import TodoList from "./components/TodoList"
-
-export default async function Home() {
-  const tasks = await getAllTodos()
-  console.log("🚀 ~ tasks:", tasks)
-
+export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Todo List App</h1>
-        <AddTask/>
-      </div>
-
-      <TodoList tasks={tasks}/>
+    <main>
+      <h1>Home</h1>
     </main>
   )
 }
